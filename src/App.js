b5b80c33b0b85e0707f2b1dd72bda6fcea159b3c
sandeep19 <sandbro7163337@gmail.com>
2023-02-15 13:24:26 +0530

    File commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './components/Microsoft/Footer'
+import Logo from './components/Microsoft/Logo'
+import Logo2 from './components/Microsoft/Logo2'
 
-function App() {
+import Navbar from './components/Microsoft/Navbar'
+import Products from './components/Microsoft/Products'
+import Section from './components/Microsoft/Section'
+import Section2 from './components/Microsoft/Section2'
+
+
+// import {Navbar} from './components/Landing/Navbar';
+// import Section from './components/Landing/Section';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      
+        <Navbar/>
+        <Section/>
+        <Logo/>
+        <Products/>
+        <Section2/>
+        <Logo2/>
+        <Footer/>
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
